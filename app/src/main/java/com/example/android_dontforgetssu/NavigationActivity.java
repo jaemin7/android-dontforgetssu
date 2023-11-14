7e3c0f31cada9399a234e433c69dd1c3b074ba04
limjaemin <7limjaemin7@naver.com>
2023-11-15 06:17:53 +0900

    feat: 네비게이션 바 기능 구현

---
+++ app/src/main/java/com/example/android_dontforgetssu/NavigationActivity.java
@@ -0,0 +1,56 @@
+package com.example.android_dontforgetssu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.android_dontforgetssu.databinding.ActivityNavigationBinding;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    private static final String TAG_HOME = "HomeFragment";
+    private static final String TAG_MANAGEMENT= "ManagementFragment";
+    private static final String TAG_MY = "MyFragment";
+
+    private ActivityNavigationBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityNavigationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.navigationView.setSelectedItemId(R.id.home_Fragment);
+        binding.navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.home_Fragment) {
+                    replaceFragment(new HomeFragment());
+                } else if (itemId == R.id.management_Fragment) {
+                    replaceFragment(new ManagementFragment());
+                } else if (itemId == R.id.my_Fragment) {
+                    replaceFragment(new MyFragment());
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(binding.mainFrameLayout.getId(), fragment).commit();
+    }
+}
